b1425ef0fff3cb80f9c09e3428f32eb433cc5201
pisit65 <pisit.ph.65@ubu.ac.th>
2025-03-05 12:12:15 +0700

    '5'

--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Course
-from .forms import CourseSearchForm
 
 def course_search(request):
     query = request.GET.get('query', '')  # รับค่า 'query' จาก URL
     courses = Course.objects.filter(name__icontains=query) if query else []  # ค้นหาวิชาจาก query
-    return render(request, 'course_search.html', {'courses': courses, 'query': query})
+    return render(request, 'search_name.html', {'courses': courses, 'query': query})
